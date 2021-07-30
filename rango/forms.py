bfdf6de3b1b405e87ecb0516f72bdadd4a566d31
Thomas Kuhn <2606172k@student.gla.ac.uk>
2021-07-30 15:55:16 +0100

    reduced code repetition in forms.py

--- rango/forms.py
+++ rango/forms.py
@@ -4,7 +4,7 @@ from rango.models import Page, Category, UserProfile
 from django.contrib.auth.models import User
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text="Please enter the category name.")
+    name = forms.CharField(max_length=Category.NAME_MAX_LENGTH, help_text="Please enter the category name.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial = 0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -16,7 +16,7 @@ class CategoryForm(forms.ModelForm):
         
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text="Please enter the title of the page")
+    title = forms.CharField(max_length=Page.TITLE_MAX_LENGTH, help_text="Please enter the title of the page")
     url = forms.URLField(max_length=200, help_text="Please enter the URL of the page.")
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     
